cdce60f6147d4386517ad38c50f3fa8c0cb6e5e1
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-01-22 18:50:07 +0300

    completed task

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request, Respose
+
+@Request.application
+def application(request):
+    print (f"This web server is running at {request.remote_addr}")
+    return Respose('A WSGI generated this response!')
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple("localhost", 8080, application= application)
